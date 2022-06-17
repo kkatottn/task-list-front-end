3f2104786c8cccca9ce95e7bc7c7a026a68fefd5
Katherine Otten Kim <kkatherine317@gmail.com>
2022-06-17 13:36:05 -0700

    wave2 partial complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -16,13 +17,36 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const changeComplete = (id) => {
+    const tasksCopy = [...tasks];
+    const obj = tasksCopy.find((task) => task.id === id);
+    obj.isComplete = !obj.isComplete;
+
+    setTasks(tasksCopy);
+  };
+
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {
+            <TaskList
+              tasks={TASKS}
+              changeComplete={changeComplete}
+              deleteTask={deleteTask}
+            />
+          }
+        </div>
       </main>
     </div>
   );
